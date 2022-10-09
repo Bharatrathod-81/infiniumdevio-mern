a976a035c6da58ec4460a5f82010e0a4bfffe783
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-10-09 14:42:31 +0530

    update code

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
-const URL = process.env.URL;
+// const URL = process.env.URL;
 
 
 function initializeDBConnection() {
   // Connecting to DB
   try{
-      mongoose.connect(URL, {
+      mongoose.connect(process.env.URL,{
         useUnifiedTopology: true,
         useNewUrlParser: true,
+        useCreateIndex: true
       })
       console.log("successfully connected")
   }catch(err){
       console.error("mongoose connection failed...", err)
+      process.exit(1)
   };
     
 };
